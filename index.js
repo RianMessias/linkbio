b255c3267ac24d73219abd3ae303b393d6b9fe8b
Rian Messias <ogmenospam@gmail.com>
2025-01-20 02:17:23 -0300

    Primeiro commit: Projeto LinkBio

---
+++ index.js
@@ -0,0 +1,4 @@
+// Aqui você pode adicionar funcionalidades se desejar, como contadores de cliques, animações, etc.
+// No exemplo abaixo, vamos apenas adicionar um console.log para mostrar que o script está funcionando.
+
+console.log("Script carregado!");
